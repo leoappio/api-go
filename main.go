0d00109a23061ace81d0b4098fe0d9532d5758a8
leo.appio <leonardo.appio@gmail.com>
2023-12-02 16:30:00 -0300

    adicionado pacote de config

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
 	router := router.Gerar()
 
 	log.Fatal(http.ListenAndServe(":5000", router))
---
+++ src/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	ConnectionString = ""
+	Porta            = 0
+)
+
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+
+	if erro != nil {
+		Porta = 9000
+	}
+
+	ConnectionString = "aqui_vai_a_connection_string"
+}
