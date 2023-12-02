15da3cdc175d3262fc081447185284d5d9efd992
leo.appio <leonardo.appio@gmail.com>
2023-12-02 17:00:24 -0300

    criada estrutura dos repositoruios

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -1,9 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := io.ReadAll(r.Body)
 
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+
+	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repository := repositorios.NovoRepositorioDeUsuarios(db)
+	repository.Criar(usuario)
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
