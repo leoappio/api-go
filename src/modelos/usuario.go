d66b788052ee44d8f74e0b91e6f542e521a0ace1
leo.appio <leonardo.appio@gmail.com>
2023-12-03 23:15:15 -0300

    adicionado endpoint para buscar usuarios

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
@@ -9,4 +13,39 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
