2289903a1b66c7c01bb7a4a4eb07b74fd75f4e92
leo.appio <leonardo.appio@gmail.com>
2023-12-03 23:37:00 -0300

    adicionado endpoint de delete de usuario

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -117,3 +117,20 @@ func (repositorio Usuarios) Atualizar(usuarioId uint64, usuario modelos.Usuario)
 
 	return nil
 }
+
+func (repositorio Usuarios) Deletar(usuarioId uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM USUARIOS WHERE ID = ?")
+
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuarioId); erro != nil {
+		return erro
+	}
+
+	return nil
+}
