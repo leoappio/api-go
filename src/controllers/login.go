99125622f0f8a1537c701906988e4d9bdcbb4f1f
leo.appio <leonardo.appio@gmail.com>
2023-12-10 11:08:36 -0300

    adicionada rota de login

---
+++ src/controllers/login.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := io.ReadAll(r.Body)
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+
+	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repository := repositorios.NovoRepositorioDeUsuarios(db)
+
+	usuarioSalvoNoBanco, erro := repository.BuscarPorEmail(usuario.Email)
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro := seguranca.ValidatePassword(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
+}
